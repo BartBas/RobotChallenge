aff137c9dec23d0ba6e6a858fde43f9ab1d5cea7
Bart Bas <bb.elsenaar@student.avans.nl>
2026-02-03 22:48:40 +0100

    MakeFile Test

--- RBpi5/src/main.cpp
+++ RBpi5/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
+    std::cout << "Hello World! and all that matters! \n";
+    std::cout << "Please now begone! \n";
     return 0;
 }
