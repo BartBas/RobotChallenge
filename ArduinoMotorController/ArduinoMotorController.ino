52d4edb5fdcb507207f861fa85c9c46afbf7909b
Theemoo <timo.derks@gmail.com>
2026-02-04 15:52:21 +0100

    MotorControlV1

--- ArduinoMotorController/ArduinoMotorController.ino
+++ ArduinoMotorController/ArduinoMotorController.ino
@@ -1,9 +1,28 @@
+#include <Wire.h>
+#include "Adafruit_MotorShield.h"
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *myMotor = AFMS.getMotor(2);
 void setup() {
-  // put your setup code here, to run once:
-  // dit is een uitleg!
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  
+  // Set the speed to start, from 0 (off) to 255 (max speed)
+  myMotor->setSpeed(150);
+  myMotor->run(FORWARD);
+  // turn on motor
+  myMotor->run(RELEASE);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  myMotor->run(FORWARD);
+  myMotor->setSpeed(150); 
+  delay(3000);
+  myMotor->run(RELEASE);
+ 
+ // myMotor->run(BACKWARD);
+  //myMotor->setSpeed(180); 
+  //delay(2000);
+  //myMotor->run(RELEASE);
+  //delay(1000);
 }
