b1e657a9cab057a9293be197c5006113069fd97b
Theemoo <timo.derks@gmail.com>
2026-03-19 11:18:28 +0100

    working version on new frame

--- ArduinoMotorController/ArduinoMotorController.ino
+++ ArduinoMotorController/ArduinoMotorController.ino
@@ -10,15 +10,15 @@ QGPMaker_Encoder *encoders[4];
 const int PPR = 12;
 const int gearratio = 90;
 const int CPR = (PPR * 4) * gearratio;
-const unsigned long sampleTime = 100; // Interval in milliseconden (0.1 seconde)
+const unsigned long sampleTime = 100; // Interval in ms (0.1 seconde)
 
-// Correctie voor draairichting encoders per motor
+// CORRECTIE: Richting aangepast omdat motorpolariteit is omgewisseld
 int encoderDirection[4] = {-1, -1, 1, 1}; 
 
-// PID Instellingen (Pas deze waarden aan tijdens het testen)
+// PID Instellingen
 double targetRPM = 60.0; 
-double Kp = 2.5;         // Proportioneel: reageert direct op de fout
-double Ki = 2.5;         // Integraal: elimineert de blijvende afwijking
+double Kp = 2.5;         
+double Ki = 2.5;         
 
 double integralError[4] = {0, 0, 0, 0};
 unsigned long lastMillis = 0;
@@ -30,20 +30,19 @@ void setup() {
   for (int i = 0; i < 4; i++) {
     motors[i] = AFMS.getMotor(i + 1);
     encoders[i] = new QGPMaker_Encoder(i + 1);
-    motors[i]->run(FORWARD);
+    motors[i]->run(FORWARD); // Controleer of dit nog de juiste kant op is!
     motors[i]->setSpeed(0);
   }
   
-  Serial.println("Systeem gestart. Modus: Standaard PI-regeling.");
+  Serial.println("Systeem gestart. Encoderrichting gecorrigeerd.");
   lastMillis = millis();
 }
 
 void loop() {
   unsigned long currentMillis = millis();
 
-  // Voer de regeling alleen uit als de sampleTime is verstreken
   if (currentMillis - lastMillis >= sampleTime) {
-    double dt = (double)(currentMillis - lastMillis) / 1000.0; // Werkelijke tijdstap in seconden
+    double dt = (double)(currentMillis - lastMillis) / 1000.0;
     lastMillis = currentMillis;
 
     Serial.print("Target:");
@@ -51,31 +50,34 @@ void loop() {
     Serial.print(" ");
 
     for (int i = 0; i < 4; i++) {
-      // 1. Lees de encoder uit en reset direct
+      // 1. Lees de encoder uit en reset
       long pos = encoders[i]->read() * encoderDirection[i]; 
       encoders[i]->write(0);
       
       // 2. Bereken actuele RPM
-      // Formule: (ticks / CPR) * (60s / dt)
       double currentRPM = ((double)pos / (double)CPR) * (60.0 / dt);
 
       // 3. PI Regeling
       double error = targetRPM - currentRPM;
       
-      // Integraal opbouwen (met anti-windup limit om "op hol slaan" te voorkomen)
-      integralError[i] += error * dt;
-      integralError[i] = constrain(integralError[i], -100, 100); 
+      // Anti-windup & Integral reset bij stilstand
+      if (targetRPM == 0) {
+        integralError[i] = 0;
+      } else {
+        integralError[i] += error * dt;
+        integralError[i] = constrain(integralError[i], -100, 100); 
+      }
 
-      // Bereken de output (PWM waarde)
+      // Bereken output
       double output = (Kp * error) + (Ki * integralError[i]);
       
-      // Begrens de output naar geldige PWM waarden (0-255)
+      // Begrens naar 0-255 PWM
       int finalPWM = constrain((int)output, 0, 255);
 
-      // 4. Stuur de motor aan
+      // 4. Stuur motor aan
       motors[i]->setSpeed(finalPWM);
 
-      // 5. Plotter output voor deze motor
+      // 5. Plotter output
       Serial.print("M");
       Serial.print(i + 1);
       Serial.print(":");
@@ -83,6 +85,6 @@ void loop() {
       
       if (i < 3) Serial.print(" ");
     }
-    Serial.println(); // Sluit de regel af voor de plotter
+    Serial.println();
   }
 }
